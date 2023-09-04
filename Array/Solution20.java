cc83e5f7928fb0a5d14d48c8e1959ef5489e9600
Prasad Shembekar <prasadmshembekar@gmail.com>
2023-09-04 19:18:13 +0530

    Update

---
+++ Array/Solution20.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Solution20{
+    public static void main(String args[]){
+        String[]  my_array = new String[] {"Python", "JAVA", "PHP",  "Perl", "C#", "C++"};
+        ArrayList<String>  list = new ArrayList<String>(Arrays.asList(my_array));
+        System.out.println(list);
+    }
+}
