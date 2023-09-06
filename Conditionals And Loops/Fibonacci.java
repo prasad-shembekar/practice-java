e0c655930f2da4a8ad6e930a44b05979b1751f1e
Prasad Shembekar <prasadmshembekar@gmail.com>
2023-09-06 22:18:14 +0530

    Update

--- Conditionals And Loops/Fibonacci.java
+++ Conditionals And Loops/Fibonacci.java
@@ -2,6 +2,18 @@ import java.util.*;
 
 public class Fibonacci{
     public static void main(String args[]){
-        
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int p = 0;
+        int i = 1;
+        int count = 2;
+
+        while(count <= n){
+            int temp = i;
+            i + p =p;
+            p = temp;
+            count++;
+        }
+        System.out.println(b);
     }
 }
